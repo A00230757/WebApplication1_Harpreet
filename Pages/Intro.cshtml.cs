0604babca97d5ec7ef28dd1fb2f5a04d03f1c37b
A00230757 <84326658+A00230757@users.noreply.github.com>
2021-06-16 10:02:15 +0530

    Add files via upload

---
+++ Pages/Intro.cshtml.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1_Harpreet.Pages
+{
+    public class IntroModel : PageModel
+    {
+        public void OnGet()
+        {
+           
+                Result = firstNumber * secondNumber;
+                ResultSet = true;
+           
+        }
+
+        [FromQuery(Name = "firstNumber")]
+        public int firstNumber { get; set; }
+
+        [FromQuery(Name = "secondNumber")]
+        public int secondNumber { get; set; }
+
+        public int Result { get; set; }
+
+        public bool ResultSet { get; set; }
+
+    }
+}
